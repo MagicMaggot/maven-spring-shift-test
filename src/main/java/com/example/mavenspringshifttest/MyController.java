c39299adbdbad128d3d43ced36cd2f8b7073b874
Alexander Ikaev <smea9000@gmail.com>
2023-03-07 20:31:19 +0300

    t2

--- src/main/java/com/example/mavenspringshifttest/MyController.java
+++ src/main/java/com/example/mavenspringshifttest/MyController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
     @GetMapping("/")
     public String hello() {
-        return "Hello OpenShift! edit";
+        return "edit 2 Hello OpenShift!";
     }
 }
